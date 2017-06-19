b2deb89d17d90db5ec8ac690cf5208824133dbe8
Jen Schumann <jenniferschumann1@gmail.com>
2017-06-19 12:02:06 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -6,19 +6,27 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
+ cardsInPlay.push(cards[cardsInPlay]);
 
-var cardOne = cards[1];
-var cardTwo = cards[3];
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+};
 
-cardsInPlay.push('cardOne');
-console.log("User flipped " + cardOne);
-cardsInPlay.push('cardTwo');
-console.log("User flipped " + cardTwo);
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
 
 if (cardsInPlay.length === 2) {
     
-} if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else if (cardsInPlay[0] !== cardsInPlay[1]) {
-	alert("Sorry, try again.");
+checkForMatch;
 }
+
+};
+flipCard(0);
+flipCard(2);
+
+
+
