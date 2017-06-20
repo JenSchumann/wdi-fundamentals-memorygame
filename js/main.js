052c433ce64c0879fe94f3a6515d0fafa15b5a9b
Jen Schumann <jenniferschumann1@gmail.com>
2017-06-19 19:58:08 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,32 +1,53 @@
 console.log("Up and running!");
 
-
-
-
-var cards = ["queen", "queen", "king", "king"];
+// object with properties of the cards
+var cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
+	}, 
+	{
+		rank: 'queen',
+		suit:'diamonds',
+		cardImage: 'images/queen-of-diamonds.png'
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-hearts.png'
+	},
+	{
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: 'images/king-of-diamonds.png'
+	}
+	];
 
 var cardsInPlay = [];
- cardsInPlay.push(cards[cardsInPlay]);
+ cardsInPlay.push(cards[cardId].rank);
 
+// f to alert user whether match was found
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
 	} else {
 		console.log("Sorry, try again.");
-	}
-};
-
+	};
+	
+// f to randomize flipping process... eventually?
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-
-if (cardsInPlay.length === 2) {
-    
-checkForMatch;
+	console.log("User flipped " + cards[cardId].rank);
+	if (cardsInPlay.length === 2) {
+	checkForMatch();
 }
-
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+}
 };
 flipCard(0);
-flipCard(2);
+flipCard(1);
+checkForMatch(cardInPlay);
 
 
 
